fd9ee5f55e50b8e8c1cb4e708973f3cfa158e20d
laoshanxi <178029200@qq.com>
2020-05-01 10:42:39 +0800

    watcher

--- src/consul_watch/go.mod
+++ src/consul_watch/go.mod
@@ -2,4 +2,7 @@ module watcher
 
 go 1.13
 
-require github.com/hashicorp/consul/api v1.4.0
+require (
+	github.com/hashicorp/consul/api v1.4.0
+	github.com/jbrodriguez/mlog v0.0.0-20180805173533-cbd5ae8e9c53
+)
--- src/consul_watch/main.go
+++ src/consul_watch/main.go
@@ -2,37 +2,96 @@ package main
 
 import (
 	"github.com/hashicorp/consul/api"
+	"github.com/jbrodriguez/mlog"
 	"time"
+
+	"os"
 )
-import "fmt"
+
+const (
+	ConsulTopologyPath = "appmgr/topology/"
+	ConsulSecurityPath = "appmgr/security"
+)
+
+var (
+	ConsulHostTopologyKey string
+	ConsulClientObj       *api.Client
+)
 
 func main() {
-	// Get a new client
-	client, err := api.NewClient(api.DefaultConfig())
+	// init log
+	initLogging(mlog.LevelInfo, 50, 5)
+
+	// init KV key
+	hostName, _ := os.Hostname()
+	ConsulHostTopologyKey = ConsulTopologyPath + hostName
+
+	// Get a new ConsulClientObj
+	client, err := makeConsulClient()
 	if err != nil {
 		panic(err)
 	}
+	ConsulClientObj = client
 
-	// Get a handle to the KV API
-	kv := client.KV()
+	doneCh := make(chan bool)
+	go watchTopology(doneCh)
+	go watchSecurity(doneCh)
+	<-doneCh
 
-	// PUT a new KV pair
-	p := &api.KVPair{Key: "REDIS_MAXCLIENTS", Value: []byte("1000")}
-	_, err = kv.Put(p, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	queryOpt := makeQueryOptions()
-
-	// Lookup the pair
-	pair, _, err := kv.Get("REDIS_MAXCLIENTS", queryOpt)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("KV: %v %s\n", pair.Key, pair.Value)
+	mlog.Warning("exiting")
 }
 
-func makeQueryOptions() *api.QueryOptions {
-	return &api.QueryOptions{WaitTime: time.Duration(1 * time.Minute)}
+func makeConsulClient() (*api.Client, error) {
+	os.Setenv(api.HTTPAddrEnvName, "10.1.241.54:8500")
+	return api.NewClient(api.DefaultConfig())
+}
+
+func initLogging(level mlog.LogLevel, fileSizeMb int, fileRotateNum int) {
+	// https://github.com/jbrodriguez/mlog
+	if os.Getenv("LOG_NO_FILE") == "true" {
+		// Write to stdout/stderr only
+		mlog.Start(mlog.LevelInfo, "")
+	} else {
+		mbNumber := fileSizeMb
+		logPath := os.Args[0] + ".log"
+		mlog.StartEx(level, logPath, mbNumber*1024*1024, fileRotateNum)
+		mlog.Info(logPath)
+	}
+}
+
+func watchSecurity(doneCh chan bool) {
+}
+func watchTopology(doneCh chan bool) {
+	// Get a handle to the KV API
+	kv := ConsulClientObj.KV()
+
+	pair, meta, err := kv.Get(ConsulHostTopologyKey, nil)
+	if err != nil {
+		mlog.IfError(err)
+	}
+	if pair == nil {
+		mlog.Warning("no value for %#v", ConsulHostTopologyKey)
+	}
+	if meta.LastIndex == 0 {
+		mlog.Warning("unexpected meta value: %#v", meta)
+	}
+
+	// Get should work
+	options := &api.QueryOptions{WaitTime: time.Duration(30 * time.Second), WaitIndex: meta.LastIndex}
+	pair, meta2, err := kv.Get(ConsulHostTopologyKey, options)
+	if err != nil {
+		mlog.IfError(err)
+	}
+	if pair == nil {
+		mlog.Trace("no value for %#v", ConsulHostTopologyKey)
+	}
+	if meta.LastIndex == meta2.LastIndex {
+		mlog.Trace("timeout, value is not changed %#v", meta.LastIndex)
+	} else if meta2.LastIndex <= meta.LastIndex {
+		mlog.Warning("unexpected meta value: %#v , %#v", meta.LastIndex, meta2.LastIndex)
+	} else {
+		mlog.Info("Value changed")
+	}
+
+	doneCh <- true
 }
